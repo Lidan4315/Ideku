3288c8e69a8ee06b668732d15a229b62086662d8
Lidan <faiqlidan15@gmail.com>
2025-08-13 09:34:10 +0800

    feat: database workflow management

---
+++ Models/Entities/LevelApprover.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Ideku.Models.Entities
+{
+    [Table("LevelApprovers")]
+    public class LevelApprover
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("LevelId")]
+        public int LevelId { get; set; }
+
+        [ForeignKey("LevelId")]
+        public Level Level { get; set; } = null!;
+
+        [Required]
+        [Column("RoleId")]
+        public int RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; } = null!;
+
+        [Required]
+        [Column("IsPrimary")]
+        public bool IsPrimary { get; set; } = true;
+
+        [Required]
+        [Column("ApprovalLevel")]
+        public int ApprovalLevel { get; set; }
+
+        [Required]
+        [Column("CreatedAt")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+        [Column("UpdatedAt")]
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
